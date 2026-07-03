8a37b5f11329052abfeafe19413facebfe8cb542
davidbrumslg <82057955+davidbrumslg@users.noreply.github.com>
2026-07-03 16:37:48 -0300

    Add files via upload
    Trabalho final

--- CRUD com React.js
+++ CRUD com React.js
@@ -1,15 +1,12 @@
 import { useState } from "react";
 
-
 export default function App() {
   const [dados, setDados] = useState([]);
   const [nome, setNome] = useState("");
   const [idAtual, setIdAtual] = useState(null); // Armazena o ID se estiver editando
 
-
   function salvar() {
     if (!nome) return alert("Digite algo!");
-
 
     if (idAtual === null) {
       setDados([...dados, { id: Date.now(), nome }]); // Create: Adiciona um novo objeto à lista
@@ -20,30 +17,26 @@ export default function App() {
     setNome(""); // Limpa o input
   }
 
-
   function prepararEditar(item) {
     setIdAtual(item.id);
     setNome(item.nome);
   }
 
-
   function deletar(id) {
     setDados(dados.filter(item => item.id !== id)); // Delete: Filtra a lista removendo o ID selecionado
   }
 
-
   return (
     <div>
-      <input
-        type="text"
-        value={nome}
-        onChange={(e) => setNome(e.target.value)}
-        placeholder="Nome do item"
+      <input 
+        type="text" 
+        value={nome} 
+        onChange={(e) => setNome(e.target.value)} 
+        placeholder="Nome do item" 
       />
       <button onClick={salvar}>
         {idAtual === null ? "Salvar" : "Atualizar"}
       </button>
-
 
       <ul>
         {dados.map((item) => (
